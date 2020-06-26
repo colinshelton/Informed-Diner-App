50ac98c285e1eb66e7f7ab3d335e5f9cf60f9600
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-06-26 17:02:28 -0400

    annotated script file

--- createAccount.js
+++ createAccount.js
@@ -1,14 +1,18 @@
+// Variables
 const emailInput = $("#emailInput");
 const passwordInput = $("#passwordInput");
 const submit = $("#submit");
 const google = $("#google");
 
+// Function to retrieve the user inputted values within email and password text fields
 function userCreation() {
   let emailInput = $("#emailInput").val();
   let passwordInput = $("#passwordInput").val();
+  // Set both items to local storage with their respective keys. Proof of concept before use of back end server
   localStorage.setItem("Email", emailInput);
   localStorage.setItem("Password", passwordInput);
   console.log("test");
 }
 
+// Add click event listener to submit button, run userCreation upon click
 $("#submit").click(userCreation);
