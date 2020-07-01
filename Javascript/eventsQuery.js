2911620ccb4a94ea42e376c56f531389fc619be1
srmchartroom <sean@chartroomcreative.com>
2020-06-30 20:55:33 -0400

    Event listings compilation with build listings and html & js updates for classes to select on and javascript loops to update and reveal cards based on restaurants array values

--- Javascript/eventsQuery.js
+++ Javascript/eventsQuery.js
@@ -4,7 +4,7 @@
 let restaurants = [];
 
 // This is the initial zip code we'll use for the very 1st AJAX call to grab the coordinates.
-let initialZip = "";
+let initialZip = "27408";
 
 // This is the first populated current query array that houses all the Google NearBy results
 let currentQuery = [];
@@ -13,10 +13,6 @@ let currentQuery2 = [];
 
 // Google Key
 const key = "AIzaSyD9zrHku8yPl0RU8P1IVNyfAq5YYfqj4Eg"
-
-// FireBase Configuration
-
-
 
 
 // THIS WORKS TO PULL THE GOOGLE RESULTS:
@@ -38,8 +34,10 @@ const key = "AIzaSyD9zrHku8yPl0RU8P1IVNyfAq5YYfqj4Eg"
 //  D) it then builds a new currentQuery array called "currentQuery2" with the more complete dataset
 //  E) and then finally appends the "restaurants" final array we'll use for all keying and values. 
 
+// ************** GOOGLE PLACES QUERY AND OBJECT CREATION ************** //
+
 function runQuery(coordinatesAJAX) {
-    const input = "27408";
+    const input = initialZip; 
     const inputtype = "textquery";
     const queryURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=" + input + "&inputtype=" + inputtype + "&fields=name,geometry&key=" + key;
     const proxy_url = 'https://cors-anywhere.herokuapp.com/';
@@ -66,7 +64,7 @@ function runNearbySearch(coordinates) {
     };
     const nearbyURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + $.param(nearbyParams);
     // parameters: can be name, address, or phone number
-    console.log("NEW NEARBY URL:", nearbyURL);
+    // console.log("NEW NEARBY URL:", nearbyURL);
     const proxy_url = 'https://cors-anywhere.herokuapp.com/';
     const final_url2 = proxy_url + nearbyURL;
     // make AJAX request to Google Places API - GETs JSON data at the queryQRL
@@ -75,9 +73,9 @@ function runNearbySearch(coordinates) {
         url: final_url2,
         method: "GET"
     }).then(function (placesData) {
-        console.log(placesData);
+        // console.log(placesData);
         currentQuery = placesData.results;
-        console.log("currentQuery: " + currentQuery);
+        // console.log("currentQuery: " + currentQuery);
         searchPerPlace()
     });
 }
@@ -87,9 +85,10 @@ let itemsProcessed = 0;
 function searchPerPlace(placesSearchData) {
     for (let i = 0; i < currentQuery.length; i++) {
         let placeID = currentQuery[i].place_id;
-        let photoreference = currentQuery[i].photos[0].photo_reference;
-        let photo = "https://maps.googleapis.com/maps/api/place/photo?photoreference=" + photoreference;
-        console.log(photo);
+        //  let photoreference = currentQuery[i].photos[0].photo_reference;
+        // let maxwidth = currentQuery[i].photos[0].width;
+        // let photo = "https://maps.googleapis.com/maps/api/place/photo?photoreference=" + photoreference + "&maxwidth=" + maxwidth + "&key=" + key;
+        // console.log(photo);
         const placeSearchURL = "https://maps.googleapis.com/maps/api/place/details/json?place_id=" + placeID + "&key=" + key;
         const proxy_url = 'https://cors-anywhere.herokuapp.com/';
         const final_url3 = proxy_url + placeSearchURL;
@@ -98,39 +97,39 @@ function searchPerPlace(placesSearchData) {
             method: "GET"
         }).then(function (placeSearchFields) {
             itemsProcessed++;
-            console.log(placeSearchFields);
+            // console.log(placeSearchFields);
             let phoneNum = placeSearchFields.result.formatted_phone_number;
-            console.log(phoneNum);
+            // console.log(phoneNum);
             let website = placeSearchFields.result.website;
-            console.log(website);
+            // console.log(website);
             let mapsURL = placeSearchFields.result.url;
-            console.log(mapsURL);
+            // console.log(mapsURL);
             let operatingHours = placeSearchFields.result.opening_hours.periods;
-            console.log(operatingHours);
+            // console.log(operatingHours);
             let openTime = placeSearchFields.result.opening_hours.weekday_text;
-            console.log(openTime);
+            // console.log(openTime);
             let restaurantName = placeSearchFields.result.name;
-            console.log(restaurantName);
+            // console.log(restaurantName);
             let openStatus = placeSearchFields.result.opening_hours.open_now;
-            console.log(openStatus);
+            // console.log(openStatus);
             let price_level = placeSearchFields.result.price_level;
-            console.log(price_level);
+            // console.log(price_level);
             let rating = placeSearchFields.result.rating;
-            console.log(rating);
+            // console.log(rating);
             let address = placeSearchFields.result.formatted_address;
-            console.log(address);
+            // console.log(address);
             let locationLat = placeSearchFields.result.geometry.location.lat;
-            console.log(locationLat);
+            // console.log(locationLat);
             let locationLon = placeSearchFields.result.geometry.location.lng;
-            console.log(locationLon);
+            // console.log(locationLon);
             let viewportNElat = placeSearchFields.result.geometry.viewport.northeast.lat;
-            console.log(viewportNElat);
+            // console.log(viewportNElat);
             let viewportNElon = placeSearchFields.result.geometry.viewport.northeast.lng;
-            console.log(viewportNElon);
+            // console.log(viewportNElon);
             let viewportSWlat = placeSearchFields.result.geometry.viewport.southwest.lat;
-            console.log(viewportSWlat);
+            // console.log(viewportSWlat);
             let viewportSWlon = placeSearchFields.result.geometry.viewport.southwest.lon;
-            console.log(viewportSWlon);
+            // console.log(viewportSWlon);
             currentQuery2[i] = {
                 "uid": placeID,
                 "phoneNum": phoneNum,
@@ -143,7 +142,7 @@ function searchPerPlace(placesSearchData) {
                 "price_level": price_level,
                 "rating": rating,
                 "address": address,
-                "photo": photo,
+                // "photo": photo,
                 "locationLat": locationLat,
                 "locationLon": locationLon,
                 "viewportNElat": viewportNElat || '',
@@ -152,6 +151,10 @@ function searchPerPlace(placesSearchData) {
                 "viewportSWlon": viewportSWlon || '',
                 "favorite": false,
                 "menuURL": '',
+                "safetyScore" : "",
+                "busTime" : "",
+                "leastBusTime" : "",
+                "percentBus" : ""
             }
             // set the object to the script-specific array of restaurants  
             restaurants[i] = currentQuery2[i];
@@ -161,6 +164,7 @@ function searchPerPlace(placesSearchData) {
             if (itemsProcessed === currentQuery2.length) {
                 console.log('ITEMS PROCESSED', itemsProcessed, currentQuery2.length)
                 saveRestaurants(restaurants)
+                createListings();
             }
         });
     }
@@ -169,6 +173,271 @@ function searchPerPlace(placesSearchData) {
 // The function to run the query
 runQuery();
 
+// ************** CREATE EVENTS ************** //
+function createListings () {
+    
+    for (let i=0; i < restaurants.length; i++) {
+        // unhide appropriate div
+        let thisDiv = ".main-" + (i+1);
+        console.log(thisDiv);
+        $(thisDiv).removeClass("display-none");
+
+        // show / hide favorites icon on listing
+        let thisFav = "#favorite-" + (i+1);
+        if(restaurants[i].favorite == true) {
+            $(thisFav).removeClass("display-none");
+        } else {
+            $(thisFav).addClass("display-none");
+        }
+
+        // update image
+        // let thisImgURL = '"#restImga-' + i + '"';
+        // let thisImgSRC = '"#restImage-' + i + '"';
+        // $(thisImgURL).attr("href", restaurants[i].website);
+        // $(thisImgSRC).attr("src", restaurants[i].photo);
+
+        // update Safety rating
+        let thisSafetyDef = "#safetyRatingDefault-" + (i+1);
+        let thisSafetyPop = "#safetyRatingPopulated-" + (i+1);
+        let thisSafetyRating = "#safetyRatingSpan-" + (i+1);
+        if(restaurants[i].safetyScore > 0) {
+            $(thisSafetyDef).addClass("display-none");
+            $(thisSafetyPop).removeClass("display-none");
+            $(thisSafetyRating).html(restaurants[i].safetyScore);
+        }
+
+        // Update stars rating
+        let st1e = "#star1empty-" + (i+1);
+        let st1h = "#star1half-" + (i+1);
+        let st1f = "#star1full-" + (i+1);
+        let st2e = "#star2empty-" + (i+1);
+        let st2h = "#star2half-" + (i+1);
+        let st2f = "#star2full-" + (i+1);
+        let st3e = "#star3empty-" + (i+1); 
+        let st3h = "#star3half-" + (i+1);
+        let st3f = "#star3full-" + (i+1);
+        let st4e = "#star4empty-" + (i+1);
+        let st4h = "#star4half-" + (i+1);
+        let st4f = "#star4full-" + (i+1);
+        let st5e = "#star5empty-" + (i+1);
+        let st5h = "#star5half-" + (i+1);
+        let st5f = "#star5full-" + (i+1);
+        
+        if(restaurants[i].rating == 0) {
+            $(st1e).removeClass("is-hidden"); 
+            $(st1h).addClass("is-hidden");
+            $(st1f).addClass("is-hidden");
+            $(st2e).removeClass("is-hidden");
+            $(st2h).addClass("is-hidden");
+            $(st2f).addClass("is-hidden");
+            $(st3e).removeClass("is-hidden");
+            $(st3h).addClass("is-hidden");
+            $(st3f).addClass("is-hidden");
+            $(st4e).removeClass("is-hidden");
+            $(st4h).addClass("is-hidden");
+            $(st4f).addClass("is-hidden");
+            $(st5e).removeClass("is-hidden");
+            $(st5h).addClass("is-hidden");
+            $(st5f).addClass("is-hidden");
+        } else if (restaurants[i].rating < 1) {
+            $(st1e).addClass("is-hidden");
+            $(st1h).removeClass("is-hidden");
+            $(st1f).addClass("is-hidden");
+            $(st2e).removeClass("is-hidden");
+            $(st2h).addClass("is-hidden");
+            $(st2f).addClass("is-hidden");
+            $(st3e).removeClass("is-hidden");
+            $(st3h).addClass("is-hidden");
+            $(st3f).addClass("is-hidden");
+            $(st4e).removeClass("is-hidden");
+            $(st4h).addClass("is-hidden");
+            $(st4f).addClass("is-hidden");
+            $(st5e).removeClass("is-hidden");
+            $(st5h).addClass("is-hidden");
+            $(st5f).addClass("is-hidden");
+        } else if (restaurants[i].rating == 1) {
+            $(st1e).addClass("is-hidden");
+            $(st1h).addClass("is-hidden");
+            $(st1f).removeClass("is-hidden");
+            $(st2e).removeClass("is-hidden");
+            $(st2h).addClass("is-hidden");
+            $(st2f).addClass("is-hidden");
+            $(st3e).removeClass("is-hidden");
+            $(st3h).addClass("is-hidden");
+            $(st3f).addClass("is-hidden");
+            $(st4e).removeClass("is-hidden");
+            $(st4h).addClass("is-hidden");
+            $(st4f).addClass("is-hidden");
+            $(st5e).removeClass("is-hidden");
+            $(st5h).addClass("is-hidden");
+            $(st5f).addClass("is-hidden");
+        } else if (restaurants[i].rating > 1 && restaurants[i].rating < 2 ) {
+            $(st1e).addClass("is-hidden");
+            $(st1h).addClass("is-hidden");
+            $(st1f).removeClass("is-hidden");
+            $(st2e).addClass("is-hidden");
+            $(st2h).removeClass("is-hidden");
+            $(st2f).addClass("is-hidden");
+            $(st3e).removeClass("is-hidden");
+            $(st3h).addClass("is-hidden");
+            $(st3f).addClass("is-hidden");
+            $(st4e).removeClass("is-hidden");
+            $(st4h).addClass("is-hidden");
+            $(st4f).addClass("is-hidden");
+            $(st5e).removeClass("is-hidden");
+            $(st5h).addClass("is-hidden");
+            $(st5f).addClass("is-hidden");
+        } else if (restaurants[i].rating == 2) {
+            $(st1e).addClass("is-hidden");
+            $(st1h).addClass("is-hidden");
+            $(st1f).removeClass("is-hidden");
+            $(st2e).addClass("is-hidden");
+            $(st2h).addClass("is-hidden");
+            $(st2f).removeClass("is-hidden");
+            $(st3e).removeClass("is-hidden");
+            $(st3h).addClass("is-hidden");
+            $(st3f).addClass("is-hidden");
+            $(st4e).removeClass("is-hidden");
+            $(st4h).addClass("is-hidden");
+            $(st4f).addClass("is-hidden");
+            $(st5e).removeClass("is-hidden");
+            $(st5h).addClass("is-hidden");
+            $(st5f).addClass("is-hidden");
+        } else if (restaurants[i].rating > 2 && restaurants[i].rating < 3) {
+            $(st1e).addClass("is-hidden");
+            $(st1h).addClass("is-hidden");
+            $(st1f).removeClass("is-hidden");
+            $(st2e).addClass("is-hidden");
+            $(st2h).addClass("is-hidden");
+            $(st2f).removeClass("is-hidden");
+            $(st3e).addClass("is-hidden");
+            $(st3h).removeClass("is-hidden");
+            $(st3f).addClass("is-hidden");
+            $(st4e).removeClass("is-hidden");
+            $(st4h).addClass("is-hidden");
+            $(st4f).addClass("is-hidden");
+            $(st5e).removeClass("is-hidden");
+            $(st5h).addClass("is-hidden");
+            $(st5f).addClass("is-hidden");
+        } else if (restaurants[i].rating == 3) {
+            $(st1e).addClass("is-hidden");
+            $(st1h).addClass("is-hidden");
+            $(st1f).removeClass("is-hidden");
+            $(st2e).addClass("is-hidden");
+            $(st2h).addClass("is-hidden");
+            $(st2f).removeClass("is-hidden");
+            $(st3e).addClass("is-hidden");
+            $(st3h).addClass("is-hidden");
+            $(st3f).removeClass("is-hidden");
+            $(st4e).removeClass("is-hidden");
+            $(st4h).addClass("is-hidden");
+            $(st4f).addClass("is-hidden");
+            $(st5e).removeClass("is-hidden");
+            $(st5h).addClass("is-hidden");
+            $(st5f).addClass("is-hidden");
+        } else if (restaurants[i].rating > 3 && restaurants[i].rating < 4) {
+            $(st1e).addClass("is-hidden");
+            $(st1h).addClass("is-hidden");
+            $(st1f).removeClass("is-hidden");
+            $(st2e).addClass("is-hidden");
+            $(st2h).addClass("is-hidden");
+            $(st2f).removeClass("is-hidden");
+            $(st3e).addClass("is-hidden");
+            $(st3h).addClass("is-hidden");
+            $(st3f).removeClass("is-hidden");
+            $(st4e).addClass("is-hidden");
+            $(st4h).removeClass("is-hidden");
+            $(st4f).addClass("is-hidden");
+            $(st5e).removeClass("is-hidden");
+            $(st5h).addClass("is-hidden");
+            $(st5f).addClass("is-hidden");
+        } else if (restaurants[i].rating == 4) {
+            $(st1e).addClass("is-hidden");
+            $(st1h).addClass("is-hidden");
+            $(st1f).removeClass("is-hidden");
+            $(st2e).addClass("is-hidden");
+            $(st2h).addClass("is-hidden");
+            $(st2f).removeClass("is-hidden");
+            $(st3e).addClass("is-hidden");
+            $(st3h).addClass("is-hidden");
+            $(st3f).removeClass("is-hidden");
+            $(st4e).addClass("is-hidden");
+            $(st4h).addClass("is-hidden");
+            $(st4f).removeClass("is-hidden");
+            $(st5e).removeClass("is-hidden");
+            $(st5h).addClass("is-hidden");
+            $(st5f).addClass("is-hidden");
+        } else if (restaurants[i].rating > 4 && restaurants[i].rating < 5) {
+            $(st1e).addClass("is-hidden");
+            $(st1h).addClass("is-hidden");
+            $(st1f).removeClass("is-hidden");
+            $(st2e).addClass("is-hidden");
+            $(st2h).addClass("is-hidden");
+            $(st2f).removeClass("is-hidden");
+            $(st3e).addClass("is-hidden");
+            $(st3h).addClass("is-hidden");
+            $(st3f).removeClass("is-hidden");
+            $(st4e).addClass("is-hidden");
+            $(st4h).addClass("is-hidden");
+            $(st4f).removeClass("is-hidden");
+            $(st5e).addClass("is-hidden");
+            $(st5h).removeClass("is-hidden");
+            $(st5f).addClass("is-hidden");
+        } else {
+            $(st1e).addClass("is-hidden");
+            $(st1h).addClass("is-hidden");
+            $(st1f).removeClass("is-hidden");
+            $(st2e).addClass("is-hidden");
+            $(st2h).addClass("is-hidden");
+            $(st2f).removeClass("is-hidden");
+            $(st3e).addClass("is-hidden");
+            $(st3h).addClass("is-hidden");
+            $(st3f).removeClass("is-hidden");
+            $(st4e).addClass("is-hidden");
+            $(st4h).addClass("is-hidden");
+            $(st4f).removeClass("is-hidden");
+            $(st5e).addClass("is-hidden");
+            $(st5h).addClass("is-hidden");
+            $(st5f).removeClass("is-hidden");
+        }
+
+        // update open now text 
+        let openN = "#openNowText-" + (i+1);
+        if (restaurants[i].openStatus == true) {
+            $(openN).html("OPEN NOW");
+        } else {
+            $(openN).html("CLOSED");
+        }
+
+        // update price range
+        let pricelvl = "#priceRange-" + (i+1);
+        if (restaurants[i].price_level == 4) {
+            $(pricelvl).html("$$$$");
+        } else if (restaurants[i].price_level == 3) {
+            $(pricelvl).html("$$$");
+        } else if (restaurants[i].price_level == 2) {
+            $(pricelvl).html("$$");
+        } else if (restaurants[i].price_level == 1) {
+            $(pricelvl).html("$");
+        } else {
+            $(pricelvl).html("FREE?!?");
+        }
+
+        // update Restaurant Name
+        let restNm = "#restNameSpan-" + (i+1);
+        $(restNm).html(restaurants[i].restaurantName);
+
+        // link the directions button 
+        let mapbtn = "#mapitBtn-" + (i+1);
+        $(mapbtn).attr("href",restaurants[i].mapsURL);
+
+        // linkup the website
+        let weblink = "#website-" + (i+1);
+        let webspan = "#restWebsite-" + (i+1);
+        $(weblink).attr("href", restaurants[i].website);
+        $(webspan).html("restaurants[i].website");
+        }
+    }
 
 
 // ************** FIREBASE SCRIPT ************** //
